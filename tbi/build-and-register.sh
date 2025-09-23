d742a5eefe2a1c6b73aab877a9bf335196d57408
Ubuntu <ubuntu@ip-172-31-25-137.eu-north-1.compute.internal>
2025-09-23 11:46:55 +0000

    ksbctl tbi/build-and-register.sh: smoother deployment on EC2

--- tbi/build-and-register.sh
+++ tbi/build-and-register.sh
@@ -2,14 +2,9 @@
 
 TAG=saintpeter/bastion-9:latest
 
-if [[ -z "$(docker image list -q $TAG)" ]]; then
-  docker image build --no-cache --progress=plain --tag saintpeter/bastion-9:latest .
+if [[ -z "$(minikube image list | grep $TAG)" ]]; then
+  minikube image build --tag saintpeter/bastion-9:latest .
 
-  docker save --output image.tar $TAG
-  eval $(minikube docker-env)
-  docker load --input image.tar
-  rm image.tar
-  eval $(minikube docker-env -u)
 else
   echo "[WARN]$TAG already registered"
 fi
