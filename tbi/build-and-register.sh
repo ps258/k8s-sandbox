c89c845025325e9789f0f4c058bc09edfb7416e8
Peter Stubbs <peter@tyk.io>
2025-09-26 10:49:38 +0100

    EC2/ubuntu-deploy-sandboxes ksbctl tbi/build-and-register.sh: create the bastion image during init

--- tbi/build-and-register.sh
+++ tbi/build-and-register.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
 TAG=saintpeter/bastion-9:latest
+SCRIPTNAME=$0
+SCRIPTDIR=$(
+  cd "$(dirname $SCRIPTNAME)"
+  echo $PWD
+)
+
+cd $SCRIPTDIR
+
+echo "[INFO]Building the bastion host image in minikube. Please be patient"
 
 if [[ -z "$(minikube image list | grep $TAG)" ]]; then
   minikube image build --tag saintpeter/bastion-9:latest .
-
 else
   echo "[WARN]$TAG already registered"
 fi
