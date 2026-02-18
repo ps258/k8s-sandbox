d8639960a5a152f89da7e767f45ab2c1e0ea1a33
Peter Stubbs <peter@tyk.io>
2026-02-18 12:51:12 +0000

    status got bastion build working and fixed plugin bundle delivery for k3s

--- tbi/k3s-image-build.sh
+++ tbi/k3s-image-build.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
-# save as k3s-image-build.sh
 
-if [ $# -lt 2 ]; then
-    echo "Usage: $0 <image-name:tag> <build-context>"
-    exit 1
+TAG=saintpeter/bastion-9:latest
+SCRIPTNAME=$0
+SCRIPTDIR=$(
+  cd "$(dirname $SCRIPTNAME)"
+  echo $PWD
+)
+
+cd $SCRIPTDIR
+
+if sudo k3s ctr images list | grep -q bastion-9:latest ; then
+  echo "[WARN]$TAG already registered in k3s"
+  # it can be removed with 'sudo k3s ctr images remove $TAG'
+else
+  echo "[INFO]Building the bastion host image in k3s. Please be patient"
+  if ! docker image list $TAG | grep -q $TAG; then
+    docker build -t $TAG .
+  fi
+  docker save $TAG | sudo k3s ctr images import -
+  echo "[INFO]Image $TAG is now available in k3s"
 fi
-
-IMAGE_NAME=$1
-BUILD_CONTEXT=$2
-
-echo "Building image $IMAGE_NAME..."
-docker build -t "$IMAGE_NAME" "$BUILD_CONTEXT"
-
-echo "Importing image to k3s..."
-docker save "$IMAGE_NAME" | sudo k3s ctr images import -
-
-echo "Image $IMAGE_NAME is now available in k3s"
-
--- tbi/minikube-image-build.sh
+++ tbi/minikube-image-build.sh
@@ -9,10 +9,10 @@ SCRIPTDIR=$(
 
 cd $SCRIPTDIR
 
-echo "[INFO]Building the bastion host image in minikube. Please be patient"
 
 if [[ -z "$(minikube image list | grep $TAG)" ]]; then
+  echo "[INFO]Building the bastion host image in minikube. Please be patient"
   minikube image build --tag saintpeter/bastion-9:latest .
 else
-  echo "[WARN]$TAG already registered"
+  echo "[WARN]$TAG already registered in minikube"
 fi
